ba2a11260022d9d877d7c1b4b1dd2390dd06ffaa
Oleksandr Lotko <lotkoolexandr@gmail.com>
2023-09-22 12:25:38 +0300

    fix

--- src/test.js
+++
@@ -1,9 +0,0 @@
-export const debounce = (func, delay) => {
-  let timer
-  return (...args) => {
-    clearTimeout(timer)
-    timer = setTimeout(() => {
-      func.apply(this, args)
-    }, delay)
-  }
-}
